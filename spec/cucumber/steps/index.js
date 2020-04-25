e51026e97ccf0e27f2a9a3e47afa85959e418cea
Colton <coltleese@gmail.com>
2020-04-24 20:40:07 -0400

    5 of 6 tests passing

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,19 +1,45 @@
+import superagent from 'superagent';
 import { When, Then } from 'cucumber';
+let request;
+let result;
+let error;
+let payload;
 
-   When('the client creates a POST request to /users', function (callback) {
-     callback(null, 'pending');
+   When('the client creates a POST request to /users', function () {
+     request = superagent('POST', 'localhost:8080/users');
    });
-   When('attaches a generic empty payload', function (callback) {
-     callback(null, 'pending');
+   When('attaches a generic empty payload', function () {
+     return undefined; 
    });
    When('sends the request', function (callback) {
-     callback(null, 'pending');
+     request
+	.then((response) => {
+	   result = response.res;
+	   callback();
+        })
+	.catch((errResponse) => {
+	  error = errResponse.response;
+	  callback();
+        });
    });
-   Then('our API should respond with a 400 HTTP status code', function (callback) {
-     callback(null, 'pending');
+   Then('our API should respond with a 400 HTTP status code', function () {
+      if (error.statusCode !== 400) {
+       throw new Error();
+      }
    });
-   Then('the payload of the response should be a JSON object', function (callback) {
-     callback(null, 'pending');
+   Then('the payload of the response should be a JSON object', function () {
+     const response = result || error;
+     // Check Content-Type header
+     const contentType = response.headers['Content-Type'] || response.headers['content-type'];
+     if (!contentType || !contentType.includes('application/json')) {
+       throw new Error('Response not of Content-Type application/json');
+     }
+     // Check it is valid JSON
+     try {
+       payload = JSON.parse(response.text);
+     } catch (e) {
+       throw new Error('Response not a valid JSON object');
+     }
    });
    Then('contains a message property which says "Payload should not be empty"', function (callback) {
      callback(null, 'pending');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import http from 'http';
 import '@babel/polyfill';
 
-const requestHandler = function (req, res) {
+const requestHandler = function(req, res) {
+  if (req.method === 'POST' && req.url === '/users'){
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({}));
+    return;
+  }
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, World! you idiots!');
 };
