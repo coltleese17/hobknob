d2e1c7333c94ffc4af6ecc5995f03da327faba78
Colton <coltleese@gmail.com>
2020-04-25 16:12:37 -0400

    refactored to express, added body-parser

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,42 +1,57 @@
-import superagent from 'superagent';
 import assert from 'assert';
+import superagent from 'superagent';
 import { When, Then } from 'cucumber';
 
-   When('the client creates a POST request to /users', function () {
-     this.request = superagent('POST', `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}/users`);
-   });
-   When('attaches a generic empty payload', function () {
-     return undefined; 
-   });
-   When('sends the request', function (callback) {
+When(/^the client creates a (GET|POST|PATCH|PUT|DELETE|OPTIONS|HEAD) request to ([/\w-:.]+)$/, function (method, path) {
+  this.request = superagent(method,
+    `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}${path}`);
+});
+When(/^attaches a generic (.+) payload$/, function (payloadType) {
+  switch (payloadType) {
+    case 'malformed':
       this.request
-	.then((response) => {
-	   this.response = response.res;
-	   callback();
-        })
-	.catch((error) => {
-	   this.response = error.response;
-	  callback();
-        });
-   });
-   Then('our API should respond with a 400 HTTP status code', function () {
-     assert.equal(this.response.statusCode, 400);
-      });
-
-   Then('the payload of the response should be a JSON object', function () {
-     // Check Content-Type header
-     const contentType = this.response.headers['Content-Type'] ||
-   this.response.headers['content-type'];
-     if (!contentType || !contentType.includes('application/json')) {
-       throw new Error('Response not of Content-Type application/json');
-     }
-     // Check it is valid JSON
-     try {
-       this.responsePayload = JSON.parse(this.response.text);
-     } catch (e) {
-       throw new Error('Response not a valid JSON object');
-     }
-     });
-   Then('contains a message property which says "Payload should not be empty"', function () {
-     assert.equal(this.responsePayload.message, 'Payload should not be empty');
-   });
+        .send('{"email": "dan@danyll.com", name: }')
+        .set('Content-Type', 'application/json');
+      break;
+    case 'non-JSON':
+      this.request
+        .send('<?xml version="1.0" encoding="UTF-8"?><email>dan@danyll.com</email>').set('Content-Type', 'text/xml');
+      break;
+    case 'empty':
+    default:
+  }
+});
+When(/^sends the request$/, function (callback) {
+  this.request
+    .then((response) => {
+      this.response = response.res;
+      callback();
+    })
+    .catch((error) => {
+      this.response = error.response;
+      callback();
+    });
+});
+Then(/^our API should respond with a ([1-5]\d{2}) HTTP status code$/,
+  function (statusCode) {
+    assert.equal(this.response.statusCode, statusCode);
+  });
+Then(/^the payload of the response should be a JSON object$/, function
+() {
+  // Check Content-Type header
+  const contentType = this.response.headers['Content-Type']
+|| this.response.headers['content-type'];
+  if (!contentType || !contentType.includes('application/json')) {
+    throw new Error('Response not of Content-Type application/json');
+  }
+  // Check it is valid JSON
+  try {
+    this.responsePayload = JSON.parse(this.response.text);
+  } catch (e) {
+    throw new Error('Response not a valid JSON object');
+  }
+});
+Then(/^contains a message property which says (?:"|')(.*)(?:"|')$/,
+  function (message) {
+    assert.equal(this.responsePayload.message, message);
+  });
